f07274b3c81992eb08f2ca29f03bfa1af4ec2406
mpoloczek <m.poloczek@dmz.pl>
2022-10-19 10:13:47 +0200

    Added TypyDanych

---
+++ src/TypyDanych.java
@@ -0,0 +1,22 @@
+public class TypyDanych {
+    public static void main(String[] args) {
+
+        //Liczby całkowite
+        System.out.println(10);
+
+        //Liczby zmiennoprzecinkowe
+        System.out.println(10.5);
+
+        //Tekst
+        System.out.println("Jakiś tekst");
+
+        //Pojedyńczy znak
+        System.out.println('a');
+
+        //Prawda
+        System.out.println(true);
+
+        //Fałsz
+        System.out.println(false);
+    }
+}
