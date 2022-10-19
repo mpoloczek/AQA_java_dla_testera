73b65273eadb1b3143c2bbb6f4f45d5be67c491f
mpoloczek <m.poloczek@dmz.pl>
2022-10-19 10:22:41 +0200

    Added Zmienne

---
+++ src/Zmienne.java
@@ -0,0 +1,31 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        //Zmienna dla liczby całkowitej
+        int number10; //deklaracja zmiennej
+        number10 = 10; // inicjalizacja zmiennej
+        System.out.println(number10);
+
+        int number11 = 11; //deklaracja + inicjalizacja zmiennej
+        System.out.println(number11);
+
+        //Zmienna dla liczby zmiennoprzecinkowej
+        double doubleNumber = 10.5;
+        System.out.println(doubleNumber);
+
+        //Zmienna dla tekst
+        String someText = "to jest jakiś teks";
+        System.out.println(someText);
+
+        //Zmienna dla pojedyńczy znak
+        char someChar = 's';
+        System.out.println(someChar);
+
+        //Zmienna dla prawda lub fałsz
+        boolean trueValue = true;
+        System.out.println(trueValue);
+        boolean falseValue = false;
+        System.out.println(falseValue);
+
+    }
+}
